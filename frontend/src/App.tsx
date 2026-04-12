81f92f315ad3f375d9879bc082feb3ab04c3167e
destripador2000 <davidhumbertop503@gmail.com>
2026-04-12 15:49:21 -0600

    Feat: Agregando ruta de módulos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,12 @@ import { Welcome } from './pages/Welcome';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { Dashboard } from './pages/Dashboard';
+import { VocabularyStudy } from './pages/VocabularyStudy';
+import { VerbsStudy } from './pages/VerbsStudy';
+import { IdiomsStudy } from './pages/IdiomsStudy';
+import { SynonymsStudy } from './pages/SynonymsStudy';
+import { SayingsStudy } from './pages/SayingsStudy';
+import { CountriesStudy } from './pages/CountriesStudy';
 
 function App() {
   return (
@@ -12,6 +18,12 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/vocabulary" element={<VocabularyStudy />} />
+        <Route path="/dashboard/verbs" element={<VerbsStudy />} />
+        <Route path="/dashboard/idioms" element={<IdiomsStudy />} />
+        <Route path="/dashboard/synonyms" element={<SynonymsStudy />} />
+        <Route path="/dashboard/sayings" element={<SayingsStudy />} />
+        <Route path="/dashboard/countries" element={<CountriesStudy />} />
       </Routes>
     </BrowserRouter>
   );

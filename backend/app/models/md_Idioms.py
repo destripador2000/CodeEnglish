8d426677348281d06e8d3c2c95f20161772527a3
destripador2000 <davidhumbertop503@gmail.com>
2026-04-13 12:13:28 -0600

    Feat: Creando modelo Idioms para tabla en la db)

---
+++ backend/app/models/md_Idioms.py
@@ -0,0 +1,19 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
+from .base import Base
+
+# Verificamos que no hayan relaciones redundantes
+if TYPE_CHECKING:
+    from .md_Pages import Page
+
+
+# Creamos tabla countries para base de datos
+class Idiom(Base):
+    __tablename__ = "idioms"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    pages_id: Mapped[int] = mapped_column(ForeignKey("pages.id"))
+    phrase: Mapped[str] = mapped_column(String(30), index=True, nullable=False)
+    meaning: Mapped[str] = mapped_column(String(255), nullable=False)
+    example: Mapped[str] = mapped_column(String(255), nullable=False)
+    page: Mapped["Page"] = relationship(back_populates="idioms")

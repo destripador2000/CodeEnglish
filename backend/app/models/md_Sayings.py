b4450d23e508694cfb85ae9a4734ada418440fba
destripador2000 <davidhumbertop503@gmail.com>
2026-04-13 12:14:00 -0600

    Feat: Creando modelo Sayings para tabla en la db)

---
+++ backend/app/models/md_Sayings.py
@@ -0,0 +1,19 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
+from .base import Base
+
+# Verificamos que no hayan relaciones redundantes
+if TYPE_CHECKING:
+    from .md_Pages import Page
+
+
+# Creamos tabla countries para base de datos
+class Saying(Base):
+    __tablename__ = "sayings"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    pages_id: Mapped[int] = mapped_column(ForeignKey("pages.id"))
+    saying: Mapped[str] = mapped_column(String(45), index=True, nullable=False)
+    meaning: Mapped[str] = mapped_column(String(255), nullable=False)
+    example: Mapped[str] = mapped_column(String(255), nullable=False)
+    page: Mapped["Page"] = relationship(back_populates="sayings")

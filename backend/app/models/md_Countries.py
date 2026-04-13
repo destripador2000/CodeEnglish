e2e057453072c01085622f539846f9d5d1252ac4
destripador2000 <davidhumbertop503@gmail.com>
2026-04-13 12:13:00 -0600

    Feat: Creando modelo Countries para tabla en la db)

---
+++ backend/app/models/md_Countries.py
@@ -0,0 +1,19 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
+from .base import Base
+
+# Verificamos que no hayan relaciones redundantes
+if TYPE_CHECKING:
+    from .md_Pages import Page
+
+
+# Creamos tabla countries para base de datos
+class Country(Base):
+    __tablename__ = "countries"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    pages_iD: Mapped[int] = mapped_column(ForeignKey("pages.id"))
+    country: Mapped[str] = mapped_column(String(30), index=True, nullable=False)
+    adjective: Mapped[str] = mapped_column(String(30), nullable=False)
+    person: Mapped[str] = mapped_column(String(30), nullable=False)
+    page: Mapped["Page"] = relationship(back_populates="countries")

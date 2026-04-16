df3646281c85a5105a227c7b61fd36b55a15374d
destripador2000 <davidhumbertop503@gmail.com>
2026-04-16 09:42:54 -0600

    Feat: Poniendo ruta de la API en archivo main. API funcional, testeo éxitoso

---
+++ backend/app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.config import settings
+from app.api.routes.rt_pages import router as router_Page
+from app.models import *
+
+app = FastAPI(title=settings.app_name)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://code-english-beta.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router_Page, prefix="/router/rt_pages", tags=["page"])
+
+
+@app.get("/")
+async def root():
+    return {"Servidor encendido"}

70188632e4b4e257c2c354e03bfcda513989e346
destripador2000 <davidhumbertop503@gmail.com>
2026-04-19 22:22:35 -0600

    Feat: Agregando ruta del endpoint post verb al main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.api.routes.rt_pages import router as router_Page
 from app.api.routes.rt_vocabularies import router as router_Vocabulary
+from app.api.routes.rt_verbs import router as router_Verb
 from app.models import *
 
 app = FastAPI(title=settings.app_name)
@@ -24,6 +25,7 @@ app.add_middleware(
 
 app.include_router(router_Page, prefix="/router/rt_pages", tags=["page"])
 app.include_router(router_Vocabulary, prefix="/router/rt_vocabularies", tags=["vocabularies"])
+app.include_router(router_Verb, prefix="/router/rt_verb", tags=["verbs"])
 
 
 @app.get("/")

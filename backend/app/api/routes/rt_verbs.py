7bb9a44b6225af800c497a2f727824fef81b7cff
destripador2000 <davidhumbertop503@gmail.com>
2026-04-20 14:59:31 -0600

    Feat: Creación de ENDPOINT GET para obtener verbos. Testeo exitoso

--- backend/app/api/routes/rt_verbs.py
+++ backend/app/api/routes/rt_verbs.py
@@ -5,12 +5,13 @@ from sqlalchemy.exc import IntegrityError
 from app.core.database import get_db
 from app.models.md_Verbs import Verb as tbl_Verb
 from app.schemas.sch_verb import VerbResponse, VerbCreate
+from sqlalchemy.future import select
 
 
 router = APIRouter()
 
 
-# API para crear una palabra de un vocabulario
+# API para crear un verbo
 @router.post("/create_verb", response_model=VerbResponse)
 async def create_verb(verb: VerbCreate,
                       conex: AsyncSession = Depends(get_db)):
@@ -31,3 +32,19 @@ async def create_verb(verb: VerbCreate,
         await conex.rollback()
         print(f"Error: {ex}")
         raise HTTPException(status_code=400, detail="Error al registrar")
+
+# API para obtener verbos
+@router.get("/get_verb")
+async def get_verb(conex: AsyncSession = Depends(get_db)):
+    try: 
+        stmt = await conex.execute(select(tbl_Verb))
+        verbs = stmt.scalars().all()
+
+        return verbs
+
+    except Exception as ex:
+        await conex.rollback()
+        print(f"Error: {ex}")
+        raise HTTPException(status_code=400, detail="Verbos no encontrados")
+
+    

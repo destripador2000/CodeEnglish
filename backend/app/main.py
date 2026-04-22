11783440929215b761044d4c1f78f89ad5727572
destripador2000 <davidhumbertop503@gmail.com>
2026-04-22 11:08:19 -0600

    Feat: Agregando ruta de ENDPOINT idioms

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,7 @@ from app.api.routes.rt_vocabularies import router as router_Vocabulary
 from app.api.routes.rt_verbs import router as router_Verb
 from app.api.routes.rt_synonyms import router as router_Synonym
 from app.api.routes.rt_sayings import router as router_Saying
+from app.api.routes.rt_idioms import router as router_Idiom
 from app.models import *
 
 app = FastAPI(title=settings.app_name)
@@ -29,8 +30,8 @@ app.include_router(router_Page, prefix="/router/rt_pages", tags=["page"])
 app.include_router(router_Vocabulary, prefix="/router/rt_vocabularies", tags=["vocabularies"])
 app.include_router(router_Verb, prefix="/router/rt_verbs", tags=["verbs"])
 app.include_router(router_Synonym, prefix="/router/rt_synonyms", tags=["synonyms"])
-app.include_router(router_Saying, prefix="/router/rt/sayings", tags=["sayings"])
-
+app.include_router(router_Saying, prefix="/router/rt_sayings", tags=["sayings"])
+app.include_router(router_Idiom, prefix="/router/rt_idioms", tags=["idioms"])
 
 @app.get("/")
 async def root():

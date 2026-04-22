f5ca809e96823311a1d19f710b371103eea7ca02
destripador2000 <davidhumbertop503@gmail.com>
2026-04-22 14:12:45 -0600

    Feat: Agregando ruta para country

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,7 @@ from app.api.routes.rt_verbs import router as router_Verb
 from app.api.routes.rt_synonyms import router as router_Synonym
 from app.api.routes.rt_sayings import router as router_Saying
 from app.api.routes.rt_idioms import router as router_Idiom
+from app.api.routes.rt_countries import router as router_Country
 from app.models import *
 
 app = FastAPI(title=settings.app_name)
@@ -32,6 +33,8 @@ app.include_router(router_Verb, prefix="/router/rt_verbs", tags=["verbs"])
 app.include_router(router_Synonym, prefix="/router/rt_synonyms", tags=["synonyms"])
 app.include_router(router_Saying, prefix="/router/rt_sayings", tags=["sayings"])
 app.include_router(router_Idiom, prefix="/router/rt_idioms", tags=["idioms"])
+app.include_router(router_Country, prefix="/router/rt_countries", tags=["countries"])
+
 
 @app.get("/")
 async def root():

40778095cd99c78641b1ee1e04e505337b87f5b6
destripador2000 <davidhumbertop503@gmail.com>
2026-04-26 17:35:04 -0600

    Feat: Creación de ENDPOINT PATCH para page

--- backend/app/api/routes/rt_pages.py
+++ backend/app/api/routes/rt_pages.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 from app.core.database import get_db
 from app.models.md_Pages import Page as tblPage
-from app.schemas.sch_page import PageResponse, PageCreate
+from app.schemas.sch_page import PageResponse, PageCreate, PageUpdate
 
 router = APIRouter()
 
@@ -24,3 +25,31 @@ async def create_page(page: PageCreate,
         await conex.rollback()
         print(f"Error:{ex}")
         raise HTTPException(status_code=400, detail="Error al registrar")
+
+
+# API para actualizar página
+@router.patch("/update_page/{id}")
+async def update_page(id: int, page: PageUpdate,
+                     conex: AsyncSession = Depends(get_db)):
+    try:
+        stmt = select(tblPage).where(tblPage.id == id)
+        result = await conex.execute(stmt)
+        upt_page = result.scalars().first()
+
+        if not upt_page:
+            raise HTTPException(status_code=400, detail="Página no encontrada")
+
+        upt_data = page.model_dump(exclude_unset=True)
+
+        for key, value in upt_data.items():
+            setattr(upt_page, key, value)
+
+        await conex.commit()
+        await conex.refresh(upt_page)
+
+        return upt_page
+
+    except Exception as ex:
+        await conex.rollback()
+        print(f"Error: {ex}")
+        raise HTTPException(status_code=500, detail="Problemas en la petición")

a561d407a3f2edee58e650a243e3382ff149e8a8
destripador2000 <davidhumbertop503@gmail.com>
2026-04-26 16:57:21 -0600

    Feat: Creando esquema update para vocabulario

--- backend/app/schemas/sch_vocabulary.py
+++ backend/app/schemas/sch_vocabulary.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 # Esquema de respuesta
@@ -18,3 +19,14 @@ class VocabularyCreate(BaseModel):
     pages_id: int
     word: str
     meaning: str
+
+
+# Esquema para actualizar
+class VocabularyUpdate(BaseModel):
+    pages_id: Optional[int] = None
+    word: Optional[str] = None
+    meaning: Optional[str] = None
+
+    model_config = {
+        "from_attributes": True
+    }

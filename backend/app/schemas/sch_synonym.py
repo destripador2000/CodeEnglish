8627128f2bdf9fdc34f91e376e33e0007f248df7
destripador2000 <davidhumbertop503@gmail.com>
2026-04-26 17:30:44 -0600

    Feat: Creación de esquema update para synonym

--- backend/app/schemas/sch_synonym.py
+++ backend/app/schemas/sch_synonym.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 # Creando esquema para reponse
@@ -18,3 +19,14 @@ class SynonymCreate(BaseModel):
     pages_id: int
     word: str
     synonym: str
+
+
+# Esquema para actualizar
+class SynonymUpdate(BaseModel):
+    pages_id: Optional[int] = None
+    word: Optional[str] = None
+    synonym: Optional[str] = None
+
+    model_config = {
+       "from_attributes": True
+    }

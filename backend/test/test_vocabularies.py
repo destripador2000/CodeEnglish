a49a0906efb63d5888e5efab35f2d89fa2f11a56
destripador2000 <davidhumbertop503@gmail.com>
2026-04-27 09:45:10 -0600

    Creación de test para ENDPOINT GET éxitoso para vocabularies

--- backend/test/test_vocabularies.py
+++ backend/test/test_vocabularies.py
@@ -28,3 +28,25 @@ def test_create_vocabulary_datos_incompletos(client):
     assert response.status_code == 422
     data = response.json()
     assert "detail" in data
+
+
+# Función para test GET (Éxitoso)
+def test_get_vocabulary_exitoso(client):
+
+    id_pagina_prueba = 5
+    payload = {
+        "pages_id": id_pagina_prueba,
+        "word": "Testing",
+        "meaning": "Pruebas"
+    }
+    client.post("/router/rt_vocabularies/create_vocabulary", json=payload)
+
+    response = client.get(f"/router/rt_vocabularies/vocabulary/{id_pagina_prueba}")
+
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+
+    assert len(data) == 1
+    assert data[0]["word"] == "Testing"
+    assert data[0]["pages_id"] == 5

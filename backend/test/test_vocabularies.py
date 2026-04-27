711fb14de3a4059337e44028afbc6c72420fe7f5
destripador2000 <davidhumbertop503@gmail.com>
2026-04-27 09:42:32 -0600

    Feat: Creando test para ENDPOINT POST pero fallido

--- backend/test/test_vocabularies.py
+++ backend/test/test_vocabularies.py
@@ -1,4 +1,4 @@
-# Función test de crear usuario
+# Función test de crear usuario (Éxitoso)
 def test_create_vocabulary(client):
 
     payload = {
@@ -14,3 +14,17 @@ def test_create_vocabulary(client):
     data = response.json()
     assert data["word"] == "Deployment"
     assert "id" in data
+
+
+# Función test de crear usuario (Error)
+def test_create_vocabulary_datos_incompletos(client):
+
+    payload_incompleto = {
+        "word": "Incompleto"
+    }
+
+    response = client.post("/router/rt_vocabularies/create_vocabulary", json=payload_incompleto)
+
+    assert response.status_code == 422
+    data = response.json()
+    assert "detail" in data

2c53270efcc2247e41b218a13a68a754285f60cd
destripador2000 <davidhumbertop503@gmail.com>
2026-04-28 13:30:31 -0600

    Feat: Creación de test fallido para ENDPOINT CREATE de idiom

--- backend/test/test_idiom.py
+++ backend/test/test_idiom.py
@@ -1,5 +1,5 @@
-# Función test de ENDPOINT CREATe (Éxitoso)
-def test_create_idiom(client):
+# Función test de ENDPOINT CREATE (Éxitoso)
+def test_create_idiom_succes(client):
 
     payload = {
         "pages_id": 1,
@@ -15,3 +15,18 @@ def test_create_idiom(client):
     data = response.json()
     assert data["phrase"] == "(To) Know by sight"
     assert "id" in data
+
+
+# Función test de ENDPOINT CREATE (Fallido)
+def test_create_idiom_fail(client):
+
+    payload = {
+        "meaning": "Conocer de vista"
+    }
+
+    response = client.post("/router/rt_idioms/create_idiom", json=payload)
+
+    assert response.status_code == 422
+    data = response.json()
+    assert "detail" in data
+    

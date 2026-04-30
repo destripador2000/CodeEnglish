97df8d4b8495d5902510745fbc689bd19331e537
destripador2000 <davidhumbertop503@gmail.com>
2026-04-30 10:34:16 -0600

    Refactor: Agregando parametros de validación de longitud para los esquemas Create y Update de verb

--- backend/app/schemas/sch_verb.py
+++ backend/app/schemas/sch_verb.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 
@@ -21,24 +21,20 @@ class VerbResponse(BaseModel):
 # Esquema para crear verbo
 class VerbCreate(BaseModel):
     page_id: int
-    base_form: str
-    meaning: str
-    present: str
-    simple_past: str
-    present_part: str
-    past_part: str
+    base_form: str = Field(max_length=25)
+    meaning: str = Field(max_length=25)
+    present: str = Field(max_length=25)
+    simple_past: str = Field(max_length=25)
+    present_part: str = Field(max_length=25)
+    past_part: str = Field(max_length=25)
 
 
 # Esquema para actualizar
 class VerbUpdate(BaseModel):
-    page_id: Optional[int] = None
-    base_form: Optional[str] = None
-    meaning: Optional[str] = None
-    present: Optional[str] = None
-    simple_past: Optional[str] = None
-    present_part: Optional[str] = None
-    past_part: Optional[str] = None
-
-    model_config = {
-       "from_attributes": True
-    }
+    page_id: Optional[int] = Field(default=None)
+    base_form: Optional[str] = Field(default=None, max_length=30)
+    meaning: Optional[str] = Field(defualt=None, max_length=30)
+    present: Optional[str] = Field(default=None, max_length=30)
+    simple_past: Optional[str] = Field(default=None, max_length=30)
+    present_part: Optional[str] = Field(default=None, max_length=30)
+    past_part: Optional[str] = Field(default=None, max_length=30)

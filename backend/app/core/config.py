c100fc9f96235205cfbae05625831e0c8f112a7c
destripador2000 <davidhumbertop503@gmail.com>
2026-04-30 09:20:17 -0600

    Feat: Agregando variables de url para frontend

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     admin_email: str
     database_url: str
     database_ram_url: str
+    url_localhost_frontend: str
+    url_alternativa_frontend: str
+    url_frontend_produccion: str
     model_config = SettingsConfigDict(env_file=".env", extra="forbid")
     secret_key: str
     algorithm: str

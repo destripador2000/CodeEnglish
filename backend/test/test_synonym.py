8a8e95c494a0bb0934622980265e2b86ac9db912
destripador2000 <davidhumbertop503@gmail.com>
2026-05-05 13:35:10 -0600

    Feat: Creación de test éxitoso para ENDPOINT GET de synonym, test fallido (Error con try except)

--- backend/test/test_synonym.py
+++ backend/test/test_synonym.py
@@ -1,5 +1,5 @@
 # Función test para crear un synonym (Éxitoso)
-def test_create_synonym(client):
+def test_create_synonym_succes(client):
     payload={
         "pages_id": 1,
         "word": "Test",
@@ -26,3 +26,25 @@ def test_create_synonym_fail(client):
     assert response.status_code == 422
     data = response.json()
     assert "detail" in data
+ 
+
+# Función test para obtener synonym (Éxitoso)
+def test_get_synonym_succes(client):
+    
+    id_test_page = 5
+    payload={
+        "pages_id": id_test_page,
+        "word": "Test",
+        "synonym": "Quiz"
+    }
+
+    client.post("/api/rt_synonyms/create_synonym", json=payload)
+    response = client.get(f"/api/rt_synonyms/synonyms/{id_test_page}")
+
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+
+    assert len(data) == 1
+    assert data[0]["word"] == "Test"
+    assert data[0]["pages_id"] == 5
